2a6404c9f94c0f654b9c731ecc539cecf07c502e
Fahri Ris Ramdhani <fahri.ramdhani@tujuhsembilan.com>
2024-05-26 22:34:09 +0700

    init

---
+++ src/main/java/demo/actuator/SpringActuatorPrometheusDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorPrometheusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorPrometheusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/actuator/SpringActuatorPrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorPrometheusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
